5bea084461e289d7e32c1b15732b5c22d6c25d4e
Rehaan12345 <rehaan1099@gmail.com>
2024-04-18 11:12:39 -0400

    more

--- drone_web/src/routes/+layout.svelte
+++ drone_web/src/routes/+layout.svelte
@@ -1,1 +1,37 @@
-<script>import "../app.pcss";</script><slot></slot>
+<script>
+  import "../app.pcss";
+  import { page } from '$app/stores';
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+  $: activeUrl = $page.url.pathname;
+  import { DarkMode } from 'flowbite-svelte';
+  import { Footer, FooterBrand, FooterCopyright, FooterIcon, FooterLink, FooterLinkGroup } from 'flowbite-svelte';
+
+  import { onNavigate } from '$app/navigation';
+
+  // onNavigate((navigation) => {
+  //   if (!document.startViewTransition) return;
+
+  //   return new Promise((resolve) => {
+  //     document.startViewTransition(async () => {
+  //       resolve();
+  //       await navigation.complete;
+  //     });
+  //   });
+  // });
+
+</script>
+
+<Navbar let:NavContainer color="primary">
+    <NavBrand href="/">
+      <DarkMode />
+      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Beantown Drones</span>
+    </NavBrand>
+    <NavHamburger />
+    <NavUl {activeUrl}>
+      <NavLi href="/">Home</NavLi>
+      <NavLi href="/Pricing">Pricing</NavLi>
+      <NavLi href="/Contact">Contact</NavLi>
+    </NavUl>
+  </Navbar>
+
+<slot></slot>
--- drone_web/src/routes/+page.svelte
+++ drone_web/src/routes/+page.svelte
@@ -1,28 +1,72 @@
 <script>
-    import { AccordionItem, Accordion } from 'flowbite-svelte';
-  </script>
+  import { Carousel } from 'flowbite-svelte';
+  import { Heading, P, Button, Mark, Span } from 'flowbite-svelte';
+  import { ArrowRightOutline } from 'flowbite-svelte-icons';
+  import { Gallery } from 'flowbite-svelte';
+  import { onMount } from "svelte";
+  import { fade, blur, fly } from 'svelte/transition';
+  import { quintOut } from 'svelte/easing';
+
+  // Intersection Obvserver functionality: (source: https://www.youtube.com/watch?v=T33NN_pPeNI)
+  let ready = false;
+  onMount(() => {
+    ready = true;
+
+    const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          console.log(entry.target);
+          entry.target.classList.add("show");
+          entry.target.classList.remove("hiddensec");
+        } else { entry.target.classList.remove("show"); entry.target.classList.add("hiddensec"); }
+      });
+    });
+  });
   
-  <Accordion>
-    <AccordionItem>
-      <span slot="header">My Header 1</span>
-      <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint explicabo ...</p>
-      <p class="text-gray-500 dark:text-gray-400">
-        Check out this guide to learn how to <a href="/" target="_blank" rel="noreferrer" class="text-blue-600 dark:text-blue-500 hover:underline"> get started </a>
-        and start developing websites even faster with components on top of Tailwind CSS.
-      </p>
-    </AccordionItem>
-    <AccordionItem>
-      <span slot="header">My Header 2</span>
-      <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint explicabo ...</p>
-      <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint explicabo ...</p>
-      <p class="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
-      <ul class="list-disc ps-5 dark:text-gray-400 text-gray-500">
-        <li>
-          <a href="/" target="_blank" rel="noreferrer" class="text-blue-600 dark:text-blue-500 hover:underline"> Lorem ipsum </a>
-        </li>
-        <li>
-          <a href="https://tailwindui.com/" rel="noreferrer" target="_blank" class="text-blue-600 dark:text-blue-500 hover:underline"> Tailwind UI </a>
-        </li>
-      </ul>
-    </AccordionItem>
-  </Accordion>
+</script>
+
+<style>
+
+.sectionsok  {
+  min-height: 100vh;
+  color: white;
+  display: grid;
+  place-content: center;
+}
+
+.hiddensec {
+  opacity: 0;
+  filter: blur(5px);
+  transform: translateX(-100%);
+  transition: all 5s;
+}
+
+</style>
+
+{#if ready}
+
+  <div class="hero min-h-screen">
+    <div class="hero-overlay bg-opacity-30"></div>
+    <div class="hero-content text-center text-neutral-content">
+      <div class="max-w-md">
+        <Heading tag="h1" class="mb-4">
+          <Span underline decorationClass="decoration-8 decoration-orange-400 dark:decoration-orange-600"><div class="part partone" transition:fly={{ delay: 0, duration: 800, x: 0, y: -500, opacity: 0, easing: quintOut }}>Custom</div></Span> 
+          <div class="part parttwo" transition:fly={{ delay: 500, duration: 800, x: -1000, y: 0, opacity: 0, easing: quintOut }}>drone footage for </div>
+          <Span underline decorationClass="decoration-8 decoration-orange-400 dark:decoration-orange-600"><div class="part partthree" transition:fly={{ delay: 700, duration: 800, x: 1000, y: 0, opacity: 0, easing: quintOut }}>every project.</div></Span> 
+        </Heading>
+        <P><center transition:blur={{ delay: 800, duration: 600, amount: 10 }}>Beantown drones combines high-quality aerial photo and video with a customized portfolio for every client, tailor made to fit your goal.</center></P>
+        <br>
+        <div class="part partfour" transition:fly={{ delay: 900, duration: 800, x: 0, y: 500, opacity: 0, easing: quintOut }}><Button href="/#section1">Scroll Down</Button></div>
+      </div>
+    </div>
+  </div>
+	
+{/if}
+
+<section out:blur={{ duration: 400 }} in:blur={{ delay: 400, duration: 400 }} class="sectionsok" id="section1">
+  <Heading>Section 1</Heading>
+</section>
+
+<section class="sectionsok hiddensec" id="section2">
+  <Heading>Section 2</Heading>
+</section>
