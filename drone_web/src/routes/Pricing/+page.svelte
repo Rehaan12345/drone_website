bd04478a5b3ffd53348db6b0ebb02fadaec0564e
Rehaan12345 <rehaan1099@gmail.com>
2024-04-18 13:50:25 -0400

    preliminary backend configuration

--- drone_web/src/routes/Pricing/+page.svelte
+++ drone_web/src/routes/Pricing/+page.svelte
@@ -1,5 +1,24 @@
 <script>
   import { Heading } from "flowbite-svelte";
 
+  let apiResults, URL = "http://127.0.0.1:8000/"
+
+  const getApiAsync = (URL) => {
+    fetch(URL)
+      .then((response) => response.json())
+      .then((response) => apiResults = response);
+  };
+
+  const handleClick = () => { getApiAsync(URL); }
+
+  $: apiResults;
+
 </script>
+
 <Heading>Pricing</Heading>
+
+<main>
+  <button on:click={ handleClick }>GetAPI</button>
+  
+  <div class="response">{apiResults ? JSON.stringify(apiResults, null, 2) : ""}</div>
+</main>
